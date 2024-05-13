daba079f46c5523f4b11bbe1b8da7ef591f2a6a5
Ehsan Rezaei <ehsanre@systemgroup.net>
2024-05-13 12:42:47 +0330

    2

---
+++ Data/Customer.sql
@@ -0,0 +1,19 @@
+﻿SELECT
+      C.CustomerId
+     ,C.PartyRef
+     ,C.Number
+     ,C.SecondCode
+     ,C.ExchangeCode
+     ,P.FullName
+     ,P.NationalId
+     ,ca.CustomerAddressId
+  FROM    Sls3.Customer AS C
+      JOIN Gnr3.Party AS P
+        ON C.PartyRef = P.PartyId
+      LEFT JOIN Sls3.CustomerAddress AS CA
+        ON C.CustomerId = CA.CustomerRef
+          AND
+          CA.IsDefault = 1
+
+
+
